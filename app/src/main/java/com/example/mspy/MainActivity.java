fb044dfeded86675c095d8c09523f1bcea928ae9
M A Mannan <mdmannan580@gmail.com>
2023-11-16 21:50:44 +0300

    How Set Title Text & Logo & Hide or Show Title Text

--- app/src/main/java/com/example/mspy/MainActivity.java
+++ app/src/main/java/com/example/mspy/MainActivity.java
@@ -11,11 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//how to Hide Action_Bar and Title of Action Bar
-       requestWindowFeature(Window.FEATURE_NO_TITLE);
-       getSupportActionBar().hide();
+////how to Hide Action_Bar and Title of Action Bar
+//       requestWindowFeature(Window.FEATURE_NO_TITLE);
+//       getSupportActionBar().hide();
 //how to Make Full_Screen
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//How Set Title Text & Logo
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.applogo); //set logo path
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
 
         setContentView(R.layout.activity_main);
     }
