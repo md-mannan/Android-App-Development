5ec38408dbb9a0f5c47363a68ab12259308c5291
M A Mannan <mdmannan580@gmail.com>
2023-11-16 21:01:10 +0300

    How to hide Action bar & Title

--- app/src/main/java/com/example/mspy/MainActivity.java
+++ app/src/main/java/com/example/mspy/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//how to Hide Action_Bar and Title of Action Bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
     }
